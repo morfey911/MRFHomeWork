101b390dd8b772978e579ccf942734c966c33d28
Yurii Mamurko <morfey911@gmail.com>
2015-05-06 21:47:23 +0300

    Task1.1 -> refactoring

--- Source/Task1.1/main.c
+++ Source/Task1.1/main.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define MRFSizeOf(type, value) \
-    printf("size = %lu bytes, value = %d\n", sizeof(type), value)
-
-#define MRFSizeOfFloating(type, value) \
-    printf("size = %lu bytes, value = %f\n", sizeof(type), value)
+#define MRFSizeOf(type) \
+    printf("Size of " #type " = %lu bytes\n", sizeof(type))
 
 int main() {
-    MRFSizeOf(short, SHRT_MAX);
-    MRFSizeOf(unsigned short int, USHRT_MAX);
-    MRFSizeOf(int, INT_MAX);
-    MRFSizeOf(unsigned int, 100);
-    MRFSizeOf(long, 100);
-    MRFSizeOf(unsigned long, 100);
-    
-    MRFSizeOfFloating(float, 8.5);
-    MRFSizeOfFloating(double, 9.5);
-    MRFSizeOfFloating(long double, 10.5);
+    MRFSizeOf(short);
+    MRFSizeOf(unsigned short int);
+    MRFSizeOf(int);
+    MRFSizeOf(unsigned int);
+    MRFSizeOf(long);
+    MRFSizeOf(unsigned long);
     
     return 0;
 }
