bb966c51cd223ca5a0f989ed209d13e493ee5c0e
Yurii Mamurko <morfey911@gmail.com>
2015-06-01 19:35:30 +0300

    User story for MRFHuman

--- MRFHuman/MRFHuman/Tests/MRFHumanTests.c
+++ MRFHuman/MRFHuman/Tests/MRFHumanTests.c
@@ -14,44 +14,42 @@
 #include "MRFObject.h"
 
 void MRFHumanTests() {
-    //создаем несколько разнополых людей, referenceCount равен по 1.
+    //after human was created
     MRFHuman *masha = MRFHumanCreateWithParameters("Masha", kMRFHumanFemale, 18);
     MRFHuman *vasya = MRFHumanCreateWithParameters("Vasya", kMRFHumanMale, 20);
     
+    //retain count must be equal to 1
     assert(MRFObjectGetReferenceCount(masha) == 1);
     assert(MRFObjectGetReferenceCount(vasya) == 1);
     
-    //женим их
+    //after merried
     MRFHumanGetMarried(masha, vasya);
 
-    //после свадьбы кол-во referenceCount у жены должно быть 2, у мужа все еще 1.
+    //retain count for man must be equal 1 and 2 for woman.
     assert(MRFObjectGetReferenceCount(masha) == 2);
     assert(MRFObjectGetReferenceCount(vasya) == 1);
     
-    //выведем чуток информации про наших героев
+    //print description
     MRFHumanPrintDescription(masha);
     MRFHumanPrintDescription(vasya);
     
-    //попробуем убить жену
-    MRFObjectRelease(masha);
-    assert(MRFObjectGetReferenceCount(masha) == 1);
-    MRFObjectRetain(masha);
-    
-    //рожаем ребенка
+    //after child birth
     MRFHuman *bob = MRFHumanCreateChild(masha, vasya);
+    
+    //retain count must be equal to 3
     assert(MRFObjectGetReferenceCount(bob) == 3);
     
-    //теперь разведем их, после развода referenceCount у жены 1, у мужа как и был 1.
+    //after divorce
     MRFHumanDivorce(masha);
     
+    //retain count to both partners must be equal to 1
     assert(MRFObjectGetReferenceCount(masha) == 1);
     assert(MRFObjectGetReferenceCount(vasya) == 1);
     
-    //опять выведем инфу
+    //print description
     MRFHumanPrintDescription(masha);
     MRFHumanPrintDescription(vasya);
     
-    //удалим наших героев, referenceCount будет равен 0.
     MRFObjectRelease(masha);
     MRFObjectRelease(vasya);
     MRFObjectRelease(bob);
