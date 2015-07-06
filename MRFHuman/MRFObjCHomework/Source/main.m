16513c56439b1d7cccc82299bdef36501132dab3
Yurii Mamurko <morfey911@gmail.com>
2015-07-06 14:55:12 +0300

    MRFObjCHomework -> Initial commit

---
+++ MRFHuman/MRFObjCHomework/Source/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MRFObjCHomework
+//
+//  Created by Yurii Mamurko on 06.07.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
