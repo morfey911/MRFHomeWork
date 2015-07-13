8efd782d56478f1df3775c78bf60f4ce6720bdef
Yurii Mamurko <morfey911@gmail.com>
2015-07-13 16:54:03 +0300

    MRFCarwashRoom -> initial commit

---
+++ MRFHuman/MRFObjCHomework/Source/MRFCarwash/MRFCarwashRoom/MRFCarwashRoom.h
@@ -0,0 +1,13 @@
+//
+//  MRFCarwashRoom.h
+//  MRFHuman
+//
+//  Created by Yurii Mamurko on 13.07.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import "MRFRoom.h"
+
+@interface MRFCarwashRoom : MRFRoom
+
+@end
---
+++ MRFHuman/MRFObjCHomework/Source/MRFCarwash/MRFCarwashRoom/MRFCarwashRoom.m
@@ -0,0 +1,13 @@
+//
+//  MRFCarwashRoom.m
+//  MRFHuman
+//
+//  Created by Yurii Mamurko on 13.07.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import "MRFCarwashRoom.h"
+
+@implementation MRFCarwashRoom
+
+@end
