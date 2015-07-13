a34b2fb63badadf429c383de095206557147fab5
Yurii Mamurko <morfey911@gmail.com>
2015-07-13 17:45:20 +0300

    MRFCarwashRoom -> initial commit

--- MRFHuman/MRFObjCHomework/Source/MRFCarwash/MRFCarwashRoom/MRFCarwashRoom.h
+++ MRFHuman/MRFObjCHomework/Source/MRFCarwash/MRFCarwashRoom/MRFCarwashRoom.h
@@ -7,7 +7,15 @@
 //
 
 #import "MRFRoom.h"
+#import "MRFCar.h"
 
 @interface MRFCarwashRoom : MRFRoom
+@property (nonatomic, retain, readonly) NSSet *cars;
+@property (nonatomic, assign, readonly) uint8_t carCapacity;
+
+- (instancetype)initWithHumanCapacity:(uint8_t)humanCapacity carCapacity:(uint8_t)carCapacity;
+
+- (void)addCar:(MRFCar *)car;
+- (void)removeCat:(MRFCar *)car;
 
 @end
--- MRFHuman/MRFObjCHomework/Source/MRFCarwash/MRFCarwashRoom/MRFCarwashRoom.m
+++ MRFHuman/MRFObjCHomework/Source/MRFCarwash/MRFCarwashRoom/MRFCarwashRoom.m
@@ -8,6 +8,52 @@
 
 #import "MRFCarwashRoom.h"
 
+@interface MRFCarwashRoom ()
+@property (nonatomic, retain) NSMutableSet *mutableCars;
+@property (nonatomic, assign) uint8_t carCapacity;
+@end
+
 @implementation MRFCarwashRoom
+
+@dynamic cars;
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.mutableCars = nil;
+    
+    [super dealloc];
+}
+
+- (instancetype)initWithHumanCapacity:(uint8_t)humanCapacity carCapacity:(uint8_t)carCapacity {
+    self = [super initWithHumanCapacity:humanCapacity];
+    
+    if (self) {
+        self.mutableCars = [NSMutableSet setWithCapacity:carCapacity];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSSet *)cars {
+    return [[self.mutableCars copy] autorelease];
+}
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (void)addCar:(MRFCar *)car {
+    if ([self.mutableCars count] < self.carCapacity) {
+        [self.mutableCars addObject:car];
+    }
+}
+
+- (void)removeCat:(MRFCar *)car {
+    [self.mutableCars removeObject:car];
+}
 
 @end
