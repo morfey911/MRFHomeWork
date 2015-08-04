2a21a94d2075a745c5de6eb55ca77e24b482c29d
Yurii Mamurko <morfey911@gmail.com>
2015-08-04 21:07:11 +0300

    MRFEmployeeObserver -> protocol initial commit

---
+++ MRFHuman/MRFObjCHomework/Source/MRFCarwash/Protocols/MRFEmployeeObserver.h
@@ -0,0 +1,20 @@
+//
+//  MRFEmployeeObserver.h
+//  MRFHuman
+//
+//  Created by Yurii Mamurko on 04.08.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class MRFEmployee;
+
+@protocol MRFEmployeeObserver <NSObject>
+
+@optional
+- (void)MRFEmployeeDidBecomeFree:(MRFEmployee *)employee;
+- (void)MRFEmployeeDidBecomeBusy:(MRFEmployee *)employee;
+- (void)MRFEmployeeDidPerformWorkWithObject:(id<MRFMoneyFlow>)object;
+
+@end
