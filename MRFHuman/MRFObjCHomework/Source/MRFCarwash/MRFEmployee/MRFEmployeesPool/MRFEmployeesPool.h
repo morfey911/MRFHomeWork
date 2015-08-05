90b4ec235cd251f01350d9d2ce8bf0bf344d4a0b
Yurii Mamurko <morfey911@gmail.com>
2015-08-05 21:27:05 +0300

    MRFEmployeePool -> initial commit

---
+++ MRFHuman/MRFObjCHomework/Source/MRFCarwash/MRFEmployee/MRFEmployeesPool/MRFEmployeesPool.h
@@ -0,0 +1,28 @@
+//
+//  MRFEmployeesPool.h
+//  MRFHuman
+//
+//  Created by Yurii Mamurko on 05.08.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "MRFEmployee.h"
+
+@interface MRFEmployeesPool : NSObject
+@property (nonatomic, retain, readonly) NSSet *employees;
+
++ (MRFEmployeesPool *)pool;
+
+- (void)addEmployee:(MRFEmployee *)employee;
+
+- (void)removeEmployee:(MRFEmployee *)employee;
+
+- (id)freeEmployeeWithClass:(Class)class;
+
+- (BOOL)containsEmployee:(MRFEmployee *)employee;
+
+- (NSUInteger)count;
+
+@end
---
+++ MRFHuman/MRFObjCHomework/Source/MRFCarwash/MRFEmployee/MRFEmployeesPool/MRFEmployeesPool.m
@@ -0,0 +1,80 @@
+//
+//  MRFEmployeesPool.m
+//  MRFHuman
+//
+//  Created by Yurii Mamurko on 05.08.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import "MRFEmployeesPool.h"
+
+@interface MRFEmployeesPool ()
+@property (nonatomic, retain) NSMutableSet *mutableEmployees;
+
+@end
+
+@implementation MRFEmployeesPool
+
+@dynamic employees;
+
+#pragma mark -
+#pragma mark Class methods
+
++ (MRFEmployeesPool *)pool {
+    return [[[self alloc] init] autorelease];
+}
+
+#pragma mark -
+#pragma mark Allocations and Deallocations
+
+- (instancetype)init {
+    self = [super init];
+    
+    if (self) {
+        self.mutableEmployees = [NSMutableSet set];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSSet *)employees {
+    return [[self.mutableEmployees copy] autorelease];
+}
+
+#pragma mark -
+#pragma mark Public methods
+
+- (void)addEmployee:(MRFEmployee *)employee {
+    [self.mutableEmployees addObject:employee];
+}
+
+- (void)removeEmployee:(MRFEmployee *)employee {
+    [self.mutableEmployees removeObject:employee];
+}
+
+- (id)freeEmployeeWithClass:(Class)class {
+    __block MRFEmployee *employee = nil;
+    
+    [self.mutableEmployees enumerateObjectsUsingBlock:^(id obj, BOOL *stop) {
+        employee = obj;
+        
+        if ([employee isKindOfClass:class] && employee.free == YES) {
+            *stop = YES;
+        }
+    }];
+    
+    return employee;
+}
+
+- (BOOL)containsEmployee:(MRFEmployee *)employee {
+    return [self.mutableEmployees containsObject:employee];
+}
+
+- (NSUInteger)count {
+    return [self.mutableEmployees count];
+}
+
+@end
