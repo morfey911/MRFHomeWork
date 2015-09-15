9175123ee465365b652e08953955227b3aaa8f54
Yurii Mamurko <morfey911@gmail.com>
2015-09-16 01:11:08 +0300

    removed test target and some worthless code UIViewController -> added category

---
+++ Objective-C UIKit/Sources/Library/UIViewController+MRFExtentions/UIViewController+MRFExtentions.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+MRFExtentions.h
+//  Objective-C UIKit
+//
+//  Created by Yurii Mamurko on 15.09.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (MRFExtentions)
+
++ (instancetype)controller;
+
+@end
---
+++ Objective-C UIKit/Sources/Library/UIViewController+MRFExtentions/UIViewController+MRFExtentions.m
@@ -0,0 +1,20 @@
+//
+//  UIViewController+MRFExtentions.m
+//  Objective-C UIKit
+//
+//  Created by Yurii Mamurko on 15.09.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import "UIViewController+MRFExtentions.h"
+
+@implementation UIViewController (MRFExtentions)
+
+#pragma mark -
+#pragma mark Class methods
+
++ (instancetype)controller {
+    return [[self alloc] initWithNibName:NSStringFromClass([self class]) bundle:nil];
+}
+
+@end
