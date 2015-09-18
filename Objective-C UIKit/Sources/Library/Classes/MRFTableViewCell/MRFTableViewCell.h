bc515efb7617f1055edce404a787994f8385f368
Yurii Mamurko <morfey911@gmail.com>
2015-09-18 10:56:08 +0300

    Library folder -> structure changed MRFTableViewCell -> reuseIdentifier getter implementation

---
+++ Objective-C UIKit/Sources/Library/Classes/MRFTableViewCell/MRFTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  MRFTableViewCell.h
+//  Objective-C UIKit
+//
+//  Created by Yurii Mamurko on 18.09.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// You should never expect the cell to have the reuseIdentifier you passed during initialization
+@interface MRFTableViewCell : UITableViewCell
+
+@end
