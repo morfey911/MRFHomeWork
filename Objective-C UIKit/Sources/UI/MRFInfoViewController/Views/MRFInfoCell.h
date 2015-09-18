c4bd993267e0a1e55abfff18604bcb0bcbd43e8b
Yurii Mamurko <morfey911@gmail.com>
2015-09-18 12:23:26 +0300

    MRFInfoCell -> initial commit

---
+++ Objective-C UIKit/Sources/UI/MRFInfoViewController/Views/MRFInfoCell.h
@@ -0,0 +1,17 @@
+//
+//  MRFInfoCell.h
+//  Objective-C UIKit
+//
+//  Created by Yurii Mamurko on 18.09.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import "MRFTableViewCell.h"
+
+@interface MRFInfoCell : MRFTableViewCell
+@property (nonatomic, strong)   IBOutlet    UIImageView *image;
+@property (nonatomic, strong)   IBOutlet    UILabel     *stringLabel;
+
+
+
+@end
---
+++ Objective-C UIKit/Sources/UI/MRFInfoViewController/Views/MRFInfoCell.m
@@ -0,0 +1,13 @@
+//
+//  MRFInfoCell.m
+//  Objective-C UIKit
+//
+//  Created by Yurii Mamurko on 18.09.15.
+//  Copyright (c) 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import "MRFInfoCell.h"
+
+@implementation MRFInfoCell
+
+@end
