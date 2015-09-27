8edf1ed58bfebeaa7aaa59f9d338b104a666b498
Yurii Mamurko <morfey911@gmail.com>
2015-09-27 15:04:56 +0300

    MRFInfoView -> renamed property 'removeButton' to 'editButton'

--- Objective-C UIKit/Sources/UI/MRFInfoViewController/MRFInfoView.h
+++ Objective-C UIKit/Sources/UI/MRFInfoViewController/MRFInfoView.h
@@ -10,7 +10,7 @@
 
 @interface MRFInfoView : UIView
 @property (nonatomic, strong)   IBOutlet    UITableView *tableView;
-@property (nonatomic, strong)   IBOutlet    UIButton    *removeButton;
+@property (nonatomic, strong)   IBOutlet    UIButton    *editButton;
 
 @property (nonatomic, assign, getter=isEditing)   BOOL    editing;
 
