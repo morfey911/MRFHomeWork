941de7d5ea0d3766976d7521abf7f802556d9ad6
Yurii Mamurko <morfey911@gmail.com>
2015-10-15 21:18:02 +0300

    MRFInfoCell -> moved method call from if

--- Objective-C UIKit/Sources/UI/MRFInfoViewController/Views/MRFInfoCell/MRFInfoCell.m
+++ Objective-C UIKit/Sources/UI/MRFInfoViewController/Views/MRFInfoCell/MRFInfoCell.m
@@ -19,9 +19,9 @@
 - (void)setInfo:(MRFInfoModel *)info {
     if (_info != info) {
         _info = info;
-        
-        [self fillWithModel:info];
     }
+    
+    [self fillWithModel:info];
 }
 
 #pragma mark -
