49228193b5794cc3924325a72fa7ea03bed447b5
Yurii Mamurko <morfey911@gmail.com>
2015-10-20 09:15:31 +0300

    MRFURLImageModel -> in developing

--- Objective-C UIKit/Sources/Models/MRFImageModel/MRFURLImageModel/MRFURLImageModel.m
+++ Objective-C UIKit/Sources/Models/MRFImageModel/MRFURLImageModel/MRFURLImageModel.m
@@ -8,13 +8,53 @@
 
 #import "MRFURLImageModel.h"
 
+@interface MRFURLImageModel ()
+@property (nonatomic, strong)   NSURLSession                *session;
+@property (nonatomic, strong)   NSURLSessionDownloadTask    *task;
+
+@end
+
 @implementation MRFURLImageModel
 
 #pragma mark -
-#pragma mark MRFModel
+#pragma mark Initializations and Deallocations
 
-- (void)performLoadingWithCompletion:(void (^)(UIImage *, id))completion {
-    [super performLoadingWithCompletion:completion];
+- (instancetype)initWithURL:(NSURL *)url {
+    self = [super init];
+    if (self) {
+        [self configureSession];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (void)setTask:(NSURLSessionDownloadTask *)task {
+    if (_task != task) {
+        [_task cancel];
+        _task = task;
+        [_task resume];
+    }
+}
+
+#pragma mark -
+#pragma mark MRFImageModel
+
+- (void)performLoadingWithCompletion:(void (^)(UIImage *image, id error))completion {
+    self.task = [self.session downloadTaskWithURL:self.url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+    }];
+//        [super performLoadingWithCompletion:completion];
+}
+
+#pragma mark -
+#pragma mark Private
+
+- (void)configureSession {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+    self.session = [NSURLSession sessionWithConfiguration:configuration];
 }
 
 @end
