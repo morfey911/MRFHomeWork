a3df0f7dc302abea9618ddbf6363a7c139b5bafb
Yurii Mamurko <morfey911@gmail.com>
2015-10-24 13:01:37 +0300

    MRFBaseContext, MRFFBLoginContext -> initial commit

---
+++ Objective-C UIKit/Sources/Contexts/MRFFBLoginContext/MRFFBLoginContext.h
@@ -0,0 +1,13 @@
+//
+//  MRFFBLoginContext.h
+//  Objective-C UIKit
+//
+//  Created by Yurii Mamurko on 24.10.15.
+//  Copyright © 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import "MRFBaseContext.h"
+
+@interface MRFFBLoginContext : MRFBaseContext
+
+@end
---
+++ Objective-C UIKit/Sources/Contexts/MRFFBLoginContext/MRFFBLoginContext.m
@@ -0,0 +1,13 @@
+//
+//  MRFFBLoginContext.m
+//  Objective-C UIKit
+//
+//  Created by Yurii Mamurko on 24.10.15.
+//  Copyright © 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import "MRFFBLoginContext.h"
+
+@implementation MRFFBLoginContext
+
+@end
