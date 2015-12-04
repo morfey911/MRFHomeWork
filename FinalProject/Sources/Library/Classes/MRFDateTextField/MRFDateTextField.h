a7fb94e4b902dd2deec5466ff546a589ab8b686d
Yurii Mamurko <morfey911@gmail.com>
2015-12-04 14:47:17 +0200

    MRFDateFieldTextField -> initial commit

---
+++ FinalProject/Sources/Library/Classes/MRFDateTextField/MRFDateTextField.h
@@ -0,0 +1,14 @@
+//
+//  MRFDateTextField.h
+//  Final Project
+//
+//  Created by Yurii Mamurko on 04.12.15.
+//  Copyright © 2015 Yurii Mamurko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRFDateTextField : UITextField
+@property (nonatomic, strong)   NSDate  *date;
+
+@end
